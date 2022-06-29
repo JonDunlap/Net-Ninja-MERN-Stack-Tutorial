9bee24ca8dd0cdd440b54cdee849a5c76e5029a7
Jonathan Dunlap <jonnyd2885@gmail.com>
2022-06-29 08:29:36 +0700

    Import and use remaining workout controller methods

--- Server/routes/workouts.js
+++ Server/routes/workouts.js
@@ -3,6 +3,8 @@ const {
   getWorkouts,
   createWorkout,
   getWorkout,
+  deleteWorkout,
+  updateWorkout,
 } = require('../controllers/workoutController');
 
 const router = express.Router();
@@ -17,13 +19,9 @@ router.post('/', createWorkout);
 router.get('/:id', getWorkout);
 
 // DELETE a workout
-router.delete('/:id', (req, res) => {
-  res.json({ msg: 'Delete a workout' });
-});
+router.delete('/:id', deleteWorkout);
 
 // UPDATE a workout
-router.patch('/:id', (req, res) => {
-  res.json({ msg: 'UPDATE a workout' });
-});
+router.patch('/:id', updateWorkout);
 
 module.exports = router;
