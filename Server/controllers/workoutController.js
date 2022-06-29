963761055e956047bbcdc3a3da3420a4007f2f32
Jonathan Dunlap <jonnyd2885@gmail.com>
2022-06-29 08:12:59 +0700

    Add comments

--- Server/controllers/workoutController.js
+++ Server/controllers/workoutController.js
@@ -3,6 +3,7 @@ const Workout = require('../models/workoutModel');
 
 // get all workouts
 const getWorkouts = async (req, res) => {
+  // get all workouts from the database, sort by date in descending order
   const workouts = await Workout.find({}).sort({ createdAt: -1 });
 
   res.status(200).json(workouts);
@@ -24,12 +25,14 @@ const createWorkout = async (req, res) => {
 const getWorkout = async (req, res) => {
   const { id } = req.params;
 
+  // check if the id is a valid mongoose id
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).json({ error: 'Invalid ID' });
   }
 
   const workout = await Workout.findById(id);
 
+  // if there is no workout return an error
   if (!workout) {
     return res.status(404).json({ error: 'Workout not found' });
   }
