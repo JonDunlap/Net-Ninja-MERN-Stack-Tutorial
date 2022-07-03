5538e21e798dc05e38d973fd6ff976428604ca6f
Jonathan Dunlap <jonnyd2885@gmail.com>
2022-07-03 12:03:08 +0700

    Add code to serve frontend
    - Check if env is set to production
    - Use path to serve build files

--- Server/server.js
+++ Server/server.js
@@ -29,6 +29,19 @@ mongoose
         `Connected to MongoDB and server started on http://localhost:${process.env.PORT}`
       );
     });
+
+    // Serve frontend
+    if (process.env.NODE_ENV === 'production') {
+      app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+      app.get('*', (req, res) =>
+        res.sendFile(
+          path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+        )
+      );
+    } else {
+      app.get('/', (req, res) => res.send('Please set to production'));
+    }
   })
   .catch((error) => {
     console.log(error);
