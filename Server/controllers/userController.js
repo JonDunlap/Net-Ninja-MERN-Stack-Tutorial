dfa804a07c66ccd66fc1fb65729bff9ad8c44d7c
Jonathan Dunlap <jonnyd2885@gmail.com>
2022-07-26 09:21:43 +0700

    Add functionality to create json web token

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,4 +1,16 @@
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+
+// create json web token
+const createToken = (_id) => {
+  return jwt.sign(
+    {
+      _id,
+    },
+    process.env.SECRET,
+    { expiresIn: '3d' }
+  );
+};
 
 // login user
 const loginUser = async (req, res) => {
