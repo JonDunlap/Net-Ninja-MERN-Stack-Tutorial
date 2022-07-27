b5dc5428ac690e68875b1f8c65d1a3635e80c62d
Jonathan Dunlap <jonnyd2885@gmail.com>
2022-07-27 07:53:26 +0700

    Create static login method

--- Server/models/userModel.js
+++ Server/models/userModel.js
@@ -43,4 +43,26 @@ userSchema.statics.signup = async function (email, password) {
   return user;
 };
 
+// static login method
+userSchema.statics.login = async function (email, password) {
+  // validate email and password
+  if (!email || !password) {
+    throw new Error('Please provide an email and password');
+  }
+
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw new Error('Email not found');
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    throw new Error('Invalid password');
+  }
+
+  return user;
+};
+
 module.exports = mongoose.model('User', userSchema);
