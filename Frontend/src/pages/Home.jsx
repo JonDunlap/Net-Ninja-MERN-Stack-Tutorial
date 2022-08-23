2cabc0dfe26ef0832699698ca2fa7ba1ab517094
Jonathan Dunlap <jonnyd2885@gmail.com>
2022-08-23 09:42:02 -0700

    Import and use useAuthContext hook
    - Allows authentication on the Home page

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,16 +1,20 @@
 import { useEffect } from 'react';
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 // components
 import WorkoutDetails from '../components/WorkoutDetails';
 import WorkoutForm from '../components/WorkoutForm';
-import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
 
 const Home = () => {
   const { workouts, dispatch } = useWorkoutsContext();
+  const { user } = useAuthContext();
 
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch('/api/workouts');
+      const response = await fetch('/api/workouts', {
+        headers: { Authorization: `Bearer ${user.token}` },
+      });
       const data = await response.json();
 
       if (response.ok) {
@@ -18,8 +22,10 @@ const Home = () => {
       }
     };
 
-    fetchWorkouts();
-  }, [dispatch]);
+    if (user) {
+      fetchWorkouts();
+    }
+  }, [dispatch, user]);
 
   return (
     <div className='home'>
